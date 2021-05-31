ee646d460b770b1ed5898e96e8bb1c96256c8a9c
kally <kallyasmedia@gmail.com>
2021-05-31 15:30:31 +0000

    Add new details for the details page

--- app/src/main/java/com/kally/recipesapp/model/MealFavorite.java
+++ app/src/main/java/com/kally/recipesapp/model/MealFavorite.java
@@ -7,7 +7,6 @@ import androidx.annotation.NonNull;
 @Entity(tableName = "favorite")
 public class MealFavorite {
 
-    //TODO #1. Table of Favorites
     @PrimaryKey @NonNull public String idMeal;
     public String strMeal;
     public String strMealThumb;
